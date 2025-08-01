d219697c5dc3a4367b840e0d6dbf49b236667c3c
Harris Fos <harrisfs.web@gmail.com>
2025-08-01 23:59:15 +0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
+import sitemap from "@astrojs/sitemap";
+
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://bookdigest.club",
+  integrations: [
+    sitemap(),
+    // ... any other integrations
+  ],
+  // existing config…
+});
