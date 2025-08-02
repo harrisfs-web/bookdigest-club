a1f16c760d35a15f453c6e54fddde1964c9bd708
Harris Fos <harrisfs.web@gmail.com>
2025-08-02 09:25:50 +0300

    sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://bookdigest.club",
   integrations: [
-    sitemap(),
-    // ... any other integrations
+    sitemap({
+      filename: "sitemap.xml",
+    }),
   ],
-  // existing config…
+  // …other config
 });
